a903727780b29d41203577ad425ffbf61382b73a
BeatMil <anutorn@gmail.com>
2020-03-23 17:36:03 +0700

    I did it. THE BIG WALL

--- dailypro_maze.py
+++ dailypro_maze.py
@@ -7,41 +7,50 @@
 def paths_through_maze(maze):
 	current_x = 0
 	current_y = 0
-	prefered_way = False # false == go down; true == go right
-	# Fill this in.
+	x_is_at_wall = False
+	y_is_at_wall = False
+	x_big_wall = False
+	y_big_wall = False
 
 	# check where the wall is
 	# check right side
 	while True:
-		# print("Check right side")
-		if current_x == 2 and current_y == 2:
-			print("FINISH!")
+		if (x_big_wall and y_is_at_wall) or (y_big_wall and x_is_at_wall):
+			print("Wall is too BIG.")
 			break
-		elif current_x  == 2:
-			pass
-		elif maze[current_y][current_x + 1] == 1:
-			print("WALL") 
 		else:
-			print("GO RIGHT")
-		  	# move to the next location, right side
-			current_x = current_x + 1
-			print("(%s,%s)"%(current_x,current_y))
+			if current_x == 2 and current_y == 2:
+				print("FINISH!")
+				break
+			elif current_x  == 2:
+				x_is_at_wall = True
+			elif y_is_at_wall and maze[current_y][current_x + 1] == 1: 
+				x_big_wall = True
+			elif maze[current_y][current_x + 1] == 1: 
+				print("RIGHT WALL") 
+			else:
+				print("GO RIGHT")
+				# move to the next location, right side
+				current_x = current_x + 1
+				print("(%s,%s)"%(current_x,current_y))
 
-		# check down side
-		# print("Check down side")
-		if current_x == 2 and current_y == 2:
-			print("FINISH!")
-			break
-		elif current_y == 2:
-			print("CANT GO INDEX OUT OF RANGE")
-		elif maze[current_y + 1][current_x] == 1:
-			print("WALL")
-		else:
-			print("GO DOWN")
-			current_y = current_y + 1
-			print("(%s,%s)"%(current_x,current_y))
+			# check down side
+			if current_x == 2 and current_y == 2:
+				print("FINISH!")
+				break
+			elif current_y == 2:
+				y_is_at_wall = True
+			elif x_is_at_wall and maze[current_y + 1][current_x] == 1: 
+				y_big_wall = True
+			elif maze[current_y + 1][current_x] == 1:
+				print("DOWN WALL")
+			else:
+				print("GO DOWN")
+				current_y = current_y + 1
+				print("(%s,%s)"%(current_x,current_y))
+
 
 paths_through_maze([[0, 1, 0],
-                    [0, 1, 0],
-                    [0, 1, 0]])
+                    [0, 1, 1],
+                    [0, 0, 0]])
 # 2
